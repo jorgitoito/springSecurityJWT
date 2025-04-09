bebf5b1d8dfca121092e9ad9540a70926f7c808d
Jorge Mateo Romero <e_jmat01@mercadona.com>
2025-04-09 10:49:41 +0200

    refactors. More robust code. checks.

--- src/main/java/com/example/security_token/domain/service/aemet/AemetService.java
+++ src/main/java/com/example/security_token/domain/service/aemet/AemetService.java
@@ -4,7 +4,6 @@ import com.example.security_token.api.aemet.exception.AemetServiceException;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
